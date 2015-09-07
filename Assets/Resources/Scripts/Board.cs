abbfec7e51c444be454ed7997caad5ac2468b1cb
reibeatall <reibeatall@gmail.com>
2015-09-06 23:33:22 -0700

    After gitignore, getting files up

---
+++ Assets/Resources/Scripts/Board.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour
+{
+
+	public List<Gem>gems = new List<Gem> ();
+	public int GridWidth;
+	public int GridHeight;
+	public GameObject gemPrefab;
+	// Use this for initialization
+	void Start ()
+	{
+		for (int y=0; y<GridHeight; y++) {
+			for (int x=0; x<GridWidth; x++) {
+				GameObject g = Instantiate (gemPrefab, new Vector3 (x, y, 0), Quaternion.identity)as GameObject;
+				g.transform.parent = gameObject.transform;
+				gems.Add (g.GetComponent<Gem> ());
+
+			}
+		}
+	
+		gameObject.transform.position = new Vector3 (-2.7f, -1.94f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/Resources/Scripts/Gem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Gem : MonoBehaviour
+{
+
+	public List<Gem> Neighbors = new List<Gem> ();
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void AddNeighbor (Gem g)
+	{
+		Neighbors.Add (g);
+	}
+
+	public void RemoveNeighbor (Gem g)
+	{
+		Neighbors.Remove (g);
+
+	}
+	void OnMouseDown ()
+	{
+
+	}
+}
